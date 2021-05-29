e0521b74c8cfe5ed677b972ab479f7fa8f36c468
Ekaterina-Lavrinenko <lavrinenko.ey@gmail.com>
2021-05-29 19:32:10 +0100

    Добавляет смену цвета при нажатии на кнопку Start

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import colors from './js/colors';
+
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+};
+
+// const colors = [
+//   '#FFFFFF',
+//   '#2196F3',
+//   '#4CAF50',
+//   '#FF9800',
+//   '#009688',
+//   '#795548',
+// ];
+
+let start = null;
+
+refs.startBtn.disabled = false;
+refs.body.style.backgroundColor = null;
+
+
+refs.startBtn.addEventListener('click', turnColors);
+refs.stopBtn.addEventListener('click', finishTurnColors);
+
+function turnColors() {
+  start = setInterval(() => {
+    const pickedColors = randomIntegerFromInterval(0, colors.length);
+    refs.body.style.backgroundColor = colors[pickedColors];
+  }, 500);
+
+  if (start) {
+    refs.startBtn.disabled = true;
+  }
+}
+
+function randomIntegerFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function finishTurnColors() {
+  clearInterval(start);
+  refs.startBtn.disabled = false;
+}
+
+// refs.startBtn.addEventListener('click', () => {
+//     randomIntegerFromInterval.start();
+// });
+
+// refs.stopBtn.addEventListener('click', () => {
+//     randomIntegerFromInterval.stop();
+// });
